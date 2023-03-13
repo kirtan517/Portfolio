51bf48c2f042d457293a9f816c43a66365b14525
kirtan517 <kananikirtan73@gmail.com>
2023-03-13 09:18:10 -0400

    added navbar logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../assets/Logo";
+import { navLinks } from "../constants";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+	return (
+		<nav className="w-full bg-orange-500 flex	items-center top-0 z-20">
+			<div className="w-full flex flex-row items-center bg-blue-500 max-w-7xl mx-auto">
+				<div className="flex flex-row">
+					<Link to="/">
+						<Logo className="bg-red-500 mr-3.5" />
+					</Link>
+					<p className="bg-red-500 text-white text-[16px] w-32 flex items-center">kirtan kanani</p>
+				</div>
+				<div className="w-full flex flex-row items-center bg-blue-500 max-w-7xl mx-auto">
+					<ul className="flex flex-row">
+					{navLinks.map((links)=>(
+						<li key={links.id} className = "px-10">
+							{links.title}
+						</li>
+					))}
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
 
-export default Navbar
+export default Navbar;
--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -2,5 +2,20 @@ const Constants = () => {
     return 1;
 };
 
+export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+
 export default Constants;
 
